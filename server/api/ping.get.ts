c4d935af8e3054e4bf1364927bbffd02c325af08
gogacoder <me@gogacoder.com>
2025-02-28 10:43:33 +0700

    ping

--- server/api/ping.get.ts
+++ server/api/ping.get.ts
@@ -1,12 +1,3 @@
-import { userSchema } from "../models/user";
-
 export default defineEventHandler(async (event) => {
-  const result = await readValidatedBody(event, (body) =>
-    userSchema.safeParse(body),
-  ); // or `.parse` to directly throw an error
-
-  if (!result.success) throw result.error.issues;
-
-  // User object is validated and typed!
-  return result.data;
+  return { message: "pong" };
 });
