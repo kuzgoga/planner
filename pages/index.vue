b4fe17db028945b5ca3c26da26bfcc39de7b2dff
Nikonell <rozhkovn08@gmail.com>
2025-02-28 12:49:19 +0700

    feat: icons

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,8 @@ const { data, error } = await useFetch("/api/ping");
 </script>
 
 <template>
-  <h2 class="font-bold text-blue-400">Wellcum to the Boilerplate</h2>
+  <h2 class="font-bold text-gray">Wellcum to the Boilerplate</h2>
+  <Icon name="material-symbols:chevron-left-rounded" />
 </template>
 
 <style scoped></style>
