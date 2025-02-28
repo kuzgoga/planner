5672518cf63c3242703a3ad9a1a88fe5836aa7d9
gogacoder <me@gogacoder.com>
2025-02-28 23:40:28 +0700

    feat: delete events endpoint

--- server/api/event/.delete.ts
+++ server/api/event/.delete.ts
@@ -1,9 +1,20 @@
 // implement delete event
 import { H3Event, EventHandlerRequest } from "h3";
+import { Event } from "../../entities/event.entity";
 import { createTypedRoute } from "../../utils/typed_route";
+import { DeleteEventResponse } from "~/server/models/events_routes";
 
-async function deleteEvent(event: H3Event) {
-  return "Not implemented";
+async function deleteEvent(event: H3Event): Promise<DeleteEventResponse> {
+  const targetId = getRequiredQueryParam(event, "id", castToNumber);
+  const item = await Event.findOneBy({
+    id: targetId,
+  });
+
+  if (!item) {
+    throw createError({ statusCode: 404, statusMessage: "Event not found" });
+  }
+
+  await Event.delete(targetId);
 }
 
 export default createTypedRoute(deleteEvent);
