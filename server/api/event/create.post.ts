5509cbd72bce2a8889448d623aa5c0058eee142f
gogacoder <me@gogacoder.com>
2025-02-28 18:05:20 +0700

    feat: create post endpoint

--- server/api/event/create.post.ts
+++ server/api/event/create.post.ts
@@ -1,5 +1,6 @@
 import { EventCreateSchema } from "../../models/create_event";
 import { Event } from "../../entities/event.entity";
+import { EventCreateResponse } from "../../models/create_event";
 
 export default defineEventHandler(async (event) => {
   const result = await readValidatedBody(event, (body) =>
@@ -9,4 +10,21 @@ export default defineEventHandler(async (event) => {
   if (!result.success) throw result.error.issues;
 
   const eventData = result.data;
+
+  const participants = await User.findByIds(eventData.userIds);
+
+  const newEvent = Event.create({
+    title: eventData.title,
+    description: eventData.description,
+    preview_path: eventData.preview_path,
+    start: eventData.start,
+    end: eventData.end,
+    participants: participants,
+  });
+
+  await newEvent.save();
+
+  const res: EventCreateResponse = {};
+
+  return res;
 });
