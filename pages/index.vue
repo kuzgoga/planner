7cabf27c0f551934a47a4618845c4a99a4078122
Nikonell <rozhkovn08@gmail.com>
2025-02-28 11:35:40 +0700

    fix: tailwind intellisense

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,7 @@
 <script setup lang="ts">
-const { data, error } = await useFetch<User[]>("/api/user", {
-  method: "POST",
-  body: {
-    sex: true,
-    token: "your_token_here",
-  } as { sex: true },
-});
+const { data, error } = await useFetch("/api/ping");
 </script>
 
 <template>
-  <h2 class="text-2xl font-bold">Wellcum to the Boilerplate</h2>
+  <h2 class="font-bold text-blue-400">Wellcum to the Boilerplate</h2>
 </template>
