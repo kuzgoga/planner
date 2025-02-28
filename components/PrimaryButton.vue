e159c229f28246988cac4d3f8f3f7e1e3cc09c01
Nikonell <rozhkovn08@gmail.com>
2025-02-28 15:35:15 +0700

    feat: input & button components

---
+++ components/PrimaryButton.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button
+    class="w-full h-[90px] flex flex-col justify-center items-center bg-accent-red text-text-gray border border-gray/65 rounded-[50px] cursor-pointer"
+  >
+    <span class="text-2xl font-extrabold">
+      <slot name="title"></slot>
+    </span>
+    <span class="text-base font-bold opacity-55 mt-[-6px]">
+      <slot name="description"></slot>
+    </span>
+  </button>
+</template>
+
+<style scoped></style>
