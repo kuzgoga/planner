0a9cd8ad57b49f4cef4dbf61c9571a5c6270836a
gogacoder <me@gogacoder.com>
2025-02-28 16:17:34 +0700

    feat: search for users

--- server/api/user.get.ts
+++ server/api/user.get.ts
@@ -1,9 +1,25 @@
+import { ILike } from "typeorm";
 import { User } from "../entities/user.entity";
 import { GetAllUsersResponse } from "../models/get_all_users";
 
 export default defineEventHandler(async (event) => {
+  /* Get all users with search by email */
+  const keyphrase = getRouterParam(event, "keyphrase");
   const _ = await requireUserSession(event);
-  const users = await User.find();
+
+  let users;
+  if (!keyphrase) {
+    users = await User.find({
+      where: [
+        { email: ILike(keyphrase!!) },
+        { firstName: ILike(keyphrase!!) },
+        { lastName: ILike(keyphrase!!) },
+      ],
+    });
+  } else {
+    users = await User.find();
+  }
+
   const response: GetAllUsersResponse = {
     users: users.map((user) => ({
       id: user.id,
