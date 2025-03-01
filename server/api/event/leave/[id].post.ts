f287be755a0feaf1a0cc953702b0c1c2b5117da1
gogacoder <me@gogacoder.com>
2025-03-01 21:16:28 +0700

    feat: leave from event

---
+++ server/api/event/leave/[id].post.ts
@@ -0,0 +1,37 @@
+import { H3Event } from "h3";
+
+import { Event } from "../../../entities/event.entity";
+
+async function joinEvent(event: H3Event): Promise<void> {
+  const session = await requireUserSession(event);
+  const eventId = getRouterParam(event, "id");
+
+  if (!eventId) {
+    createError({ statusCode: 400, statusMessage: "Invalid event ID" });
+    return;
+  }
+
+  const currentEvent = await Event.findOneBy({ id: parseInt(eventId) });
+
+  if (!currentEvent) {
+    createError({ statusCode: 404, statusMessage: "Event not found" });
+    return;
+  }
+
+  const newParticipant = await User.findOneBy({ id: session.user.id });
+
+  if (!newParticipant) {
+    createError({ statusCode: 404, statusMessage: "User not found" });
+    return;
+  }
+
+  if (currentEvent.participants) {
+    currentEvent.participants = currentEvent.participants.filter(
+      (participant) => participant.id !== session.user.id,
+    );
+  }
+
+  await currentEvent.save();
+}
+
+export default createTypedRoute(joinEvent);
